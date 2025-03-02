f2b2dbb333701a945f4d156fa9eba2aab1abb7b1
聪少 <958169644@qq.com>
2025-03-02 11:15:25 +0800

    xx

--- source/conf.py
+++ source/conf.py
@@ -14,10 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# extensions = []
-extensions = [
-    "sphinx_rtd_theme",
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
